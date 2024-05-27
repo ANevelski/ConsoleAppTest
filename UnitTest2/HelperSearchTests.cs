3523f21f5678e685400f9ab4b2b19bf9c785758a
AlakseiNevelski <Aliaksei_Nevelski@epam.com>
2024-05-27 05:53:50 +0200

    add SetUp method.

--- UnitTest2/HelperSearchTests.cs
+++ UnitTest2/HelperSearchTests.cs
@@ -2,6 +2,15 @@ namespace UnitTests
 {
     public class Tests
     {
+        private StringWriter stringWriter;
+
+        [SetUp]
+        public void CreateStringWriter()
+        {
+            stringWriter = new StringWriter();
+            Console.SetOut(stringWriter);
+        }
+
         [TestCase(null)]
         [TestCase(new int[0])]
         public void NullParametrArgCheck(int[] input)
@@ -18,9 +27,7 @@ namespace UnitTests
         public void NotNullParametrArgWithNoResult_Check()
         {
             // Arrange
-            var expectedMessage = "The indexes were not found.";
-            var stringWriter = new StringWriter();
-            Console.SetOut(stringWriter);
+            var expectedMessage = "The indexes were not found.";           
 
             // Act
             MainApp.Program.HelperSearch([0, 0, 0]);
@@ -31,15 +38,12 @@ namespace UnitTests
         }
 
         [TestCase(new int[3] { 3, 3, 13}, "The folowing indexes were found: 2") ]
+        [TestCase(new int[3] { 0, 0, 1 }, "The folowing indexes were found: 2")]
         [TestCase(new int[4] { 3, 3, 13, 33}, "The folowing indexes were found: 2; 3")]
         [TestCase(new int[5] { 3, 3, 13, 33, 0}, "The folowing indexes were found: 2; 3")]
-        [TestCase(new int[6] { 3, 3, 13, 33, 0, 67}, "The folowing indexes were found: 2; 3; 5")]
+        [TestCase(new int[6] { 3, 3, 13, 33, 0, 67}, "The folowing indexes were found: 2; 3; 5")]        
         public void NotNullParametrArgWitOnehResult_Check(int[] input, string resultMessage)
         {     
-            // Arrange
-            var stringWriter = new StringWriter();
-            Console.SetOut(stringWriter);
-
             // Act
             MainApp.Program.HelperSearch(input);
 
